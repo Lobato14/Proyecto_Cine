f31bf91716512965367919e6ae57880b75770e24
Rubén <rubencelislobato@gmail.com>
2023-05-24 23:22:31 +0200

    Creacion de funciones en clase Cine

--- src/main/kotlin/Cine.kt
+++ src/main/kotlin/Cine.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 class Cine {
     private val nFilas = 10
     private val nAsientos = 15
@@ -12,4 +14,79 @@ class Cine {
             }
         }
     }
+    // Función que muestra el cine
+    fun mostrarCine() {
+        println("-------------------------")
+        println("Cine Club Lumiere")
+        for (i in 0 until nFilas) {
+            println("fila: $i")
+            for (j in 0 until nAsientos) {
+                if (cine[i][j]) {
+                    print("XX")
+                } else {
+                    print("__")
+                }
+                print("  ")
+            }
+            println()
+        }
+        println("-------------------------")
+    }
+    // Función para vender una entrada
+    fun venderEntrada(fila: Int, asiento: Int): Boolean {
+        if (fila in 0 until nFilas && asiento in 0 until nAsientos) {
+            if (cine[fila][asiento]) {
+                println("La entrada para la fila $fila, asiento $asiento ya está vendida.")
+                return false
+            } else {
+                cine[fila][asiento] = true
+                println("Has comprado la entrada para la fila $fila, asiento $asiento.")
+                return true
+            }
+        } else {
+            println("Fila o asiento inválido.")
+            return false
+        }
+    }
+    // Función para devolver la entrada
+    fun devolverEntrada(fila: Int, asiento: Int): Boolean {
+        if (fila in 0 until nFilas && asiento in 0 until nAsientos) {
+            if (!cine[fila][asiento]) {
+                println("La entrada para la fila $fila, asiento $asiento no está vendida.")
+                return false
+            } else {
+                cine[fila][asiento] = false
+                println("Has devuelto la entrada para la fila $fila, asiento $asiento.")
+                return true
+            }
+        } else {
+            println("Fila o asiento inválido.")
+            return false
+        }
+    }
+    // Función para leer las ventas actuales
+    fun leerVentas() {
+        val file = File("entradasVendidas.txt")
+        if (file.exists()) {
+            val ventas = file.readLines()
+            for (venta in ventas) {
+                val (fila, asiento) = venta.split(":")
+                cine[fila.toInt()][asiento.toInt()] = true
+            }
+        }
+    }
+    // Funcion para guardar las ventas
+    fun guardarVentas() {
+        val file = File("entradasVendidas.txt")
+        val ventas = mutableListOf<String>()
+        for (i in 0 until nFilas) {
+            for (j in 0 until nAsientos) {
+                if (cine[i][j]) {
+                    ventas.add("$i:$j")
+                }
+            }
+        }
+        file.writeText(ventas.joinToString("\n"))
+    }
 }
+
