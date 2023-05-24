85f8bc018d9b24e0c7489434ef0872884b71971a
Rubén <rubencelislobato@gmail.com>
2023-05-24 21:18:49 +0200

    prueba

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    print("Hola")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+
+}
